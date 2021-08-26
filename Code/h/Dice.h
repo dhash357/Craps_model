25fd544cfa08f6541679ebf7f501c5a5c590952c
dhash357@gmail.com <32880094+dhash357@users.noreply.github.com>
2021-08-26 06:41:28 -0700

    Add files via upload
    Added all source code and makefile

---
+++ Code/h/Dice.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// dice.h
+class Dice
+{
+private:
+	int sides;
+
+
+public:
+	Dice(int s);
+
+	int Roll(void);
+
+};	//	Dice
---
+++ Code/src/Dice.cpp
@@ -0,0 +1,24 @@
+#include <ctime>
+#include <cstdlib>
+#include "..\h\Dice.h"
+
+
+Dice::Dice(int s) {
+		int seed;
+
+		this->sides = s;
+
+		// seed the random number function
+		seed = time(NULL);	// TODO 210808 DJH	Record the seed in the output file So we can always run the simulation again exactly the same
+		srand(seed);
+
+	}
+int Dice::Roll()
+{
+	int ret;		// Value returned from the roll of the die
+
+	ret = rand() % this->sides + 1;	// Get a random number from 1 to # of sides of the die
+	
+	return (ret);
+}
+
